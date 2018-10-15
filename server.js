4895f7a87f7a3762bbc8b405a7ab7bf88f618617
Kamil Szczypior <kszczypior1@gmail.com>
2018-10-15 12:36:32 +0200

    17-5

--- server.js
+++ server.js
@@ -4,11 +4,28 @@ var app = express();
 var bodyParser = require('body-parser');
 //app.use(bodyParser.json());
 
-app.use(express.static('assets'));
 
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store, biorę 50% z góry');
+    next();
+});
+
+app.use('/userform', function(req, res, next){
+    console.log('Jestem pośrednikiem w tym formularzu.');
+    next();
+});
+
+app.use(express.static('assets'));
+//next();
 
 app.get('/', function(req, res){
-    res.sendFile('/index.html')
+    res.sendFile('/index.html');
+    //res.send('Hello światt');
+});
+
+app.get('/store', function(req, res) {
+    res.send('To jest shop.');
 });
 
 app.get('/userform', function(req, res){
@@ -19,12 +36,21 @@ app.get('/userform', function(req, res){
     res.end(JSON.stringify(response));
 });
 
+
+
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
     var port = server.address().port;    
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
+
+app.use(function (req, res, next) {
+    res.status(404).send('Nic nie znaleziono!');
+});
+
+
+
 
 
 
